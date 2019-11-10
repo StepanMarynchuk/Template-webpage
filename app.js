19ad455147386e5e668eda4ace5225accfbca35d
StepanMarynchuk <marynchuks@gmail.com>
2019-11-10 19:31:28 +0200

    df

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function myFunction() {
+    var x = document.getElementById("myDropdown");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
