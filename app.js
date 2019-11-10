6f0ca1740742e9c88c4d2c05fb1b0f194df65094
StepanMarynchuk <marynchuks@gmail.com>
2019-11-11 00:06:53 +0200

    new version

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 "use strict"
 
-function myFunction() {
-    var x = document.getElementById("myDropdown");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+function showNavigation() {
+  let x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else if (x.style.display === "none")  {
+    x.style.display = "block";
+    x.style.display = "flex";
+    x.style.position = "absolute";
+    x.style.background = "#151515";
+    x.style.height = "100%";
+    x.style.width = "100%";
+    x.style.flexDirection = "column";
+    x.style.borderTop = "1px solid #222";
+    x.style.padding = "20px";
+    x.style.color = "red";
+    x.style.top = "60px";
+    x.style.fontSize = "50px";
+    x.style.paddingTop = "40px";
+  } else {
+    x.style.display = "none";
   }
+}
